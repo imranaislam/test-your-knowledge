218564ebef5c591a4acc63537cb57f9c007a00a2
Rajesh <rajesh.chintkuntlawar@captialone.com>
2017-11-08 22:54:28 -0500

    Removed duplicate Code

--- test-your-knowledge/src/main/java/com/capitalone/dao/DeleteDAO.java
+++
@@ -1,7 +0,0 @@
-package com.capitalone.dao;
-
-public interface DeleteDAO {
-	
-	public void delete(int questionId);
-
-}
--- test-your-knowledge/src/main/java/com/capitalone/dao/LoginDAO.java
+++
@@ -1,9 +0,0 @@
-package com.capitalone.dao;
-
-import com.capitalone.beans.User;
-
-public interface LoginDAO {
-	
-	public User login(String username, String password);
-
-}
