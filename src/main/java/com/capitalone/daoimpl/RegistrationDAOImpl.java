7ff3af65f7b534b49c3e75167de0e45e5649930c
Brunda Prakash <brunda.prakash@capitalone.com>
2017-11-17 11:31:59 -0500

    Adding encrypt and decrypt for password

--- src/main/java/com/capitalone/daoimpl/RegistrationDAOImpl.java
+++ src/main/java/com/capitalone/daoimpl/RegistrationDAOImpl.java
@@ -16,7 +16,8 @@ public class RegistrationDAOImpl implements RegistrationDAO {
 	@Override
 	public void insertUser(Registration registration) {
 		System.out.println("Registration DAO");
-		String sql = "INSERT into testyourknowledgelevel.users(user_name, password, last_name, first_name, email_id, role_id) VALUES (?,?,?,?,?,?)";
+		//String sql = "INSERT into testyourknowledgelevel.users(user_name, password, last_name, first_name, email_id, role_id) VALUES (?,?,?,?,?,?)";
+		String sql = "INSERT into testyourknowledgelevel.users(user_name, password, last_name, first_name, email_id, role_id) VALUES (?,crypt(?, gen_salt('md5')),?,?,?,?)";
 		jdbcTemplate.update(sql, registration.getUserName(), registration.getPassword(), registration.getLastName(), registration.getFirstName(), registration.getEmail(), registration.getRoleId());
 	}
 
